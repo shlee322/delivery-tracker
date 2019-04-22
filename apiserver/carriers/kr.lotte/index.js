169a5764b4258be60503742b701a001227f59504
Kyle Sanghyeok Lee <shlee322@elab.kr>
2019-04-22 16:17:42 +0900

    Fix: kr.lotte scraping error
    Fixed #13

--- apiserver/carriers/kr.lotte/index.js
+++ apiserver/carriers/kr.lotte/index.js
@@ -1,74 +1,60 @@
 const axios = require('axios')
 const { JSDOM } = require('jsdom')
-const Cookie = require('tough-cookie').Cookie;
 const qs = require('querystring')
 
-const trimString = (s) => {
-  return s.replace(/([\n\t]{1,}|\s{2,})/g, ' ').trim()
-}
-
 const parseStatus = (s) => {
-  if(s.includes('보내')) return {id: 'at_pickup', text:'상품인수'}
-  if(s.includes('배달 예정')) return {id: 'out_for_delivery', text:'배송출발'}
+  if(s.includes('상품접수')) return {id: 'at_pickup', text:'상품인수'}
+  if(s.includes('배송 출발')) return {id: 'out_for_delivery', text:'배송출발'}
   if(s.includes('배달 완료')) return {id: 'delivered', text:'배송완료'}
   return {id: 'in_transit', text: '이동중'}
 }
 
 function getTrack(trackId) {
   return new Promise((resolve, reject) => {
-    axios.post('https://www.lotteglogis.com/home/personal/inquiry/track', qs.stringify({
-      InvNo: trackId,
-      action: 'processInvoiceSubmit',
+    axios.post('https://www.lotteglogis.com/home/reservation/tracking/linkView', qs.stringify({
+      InvNo: trackId
     })).then(res => {
-      const cookie = res.headers['set-cookie'].map(Cookie.parse).map(c => c.cookieString()).join('; ')
-      return new Promise((resolve, reject) => {
-        setTimeout(() => { resolve({ cookie }) }, 2400)
-      })
-    }).then(({ cookie }) => {
-      return axios.post('https://www.lotteglogis.com/home/personal/inquiry/track', qs.stringify({
-        action: 'processInvoiceLinkSubmit',
-      }), {
-        headers: {
-          Cookie: cookie,
-        },
-      })
-    }).then(res => {
       const dom = new JSDOM(res.data)
       const document = dom.window.document
 
+      const tables = document.querySelectorAll('.tblH')
+      
       return {
-        informationTable: document.querySelector('.mat_30 .table_02'),
-        progressTable: document.querySelector('.mat_30:nth-child(2) + div .table_02')
+        informationTable: tables[0],
+        progressTable: tables[1]
       }
     }).then(({ informationTable, progressTable }) => {
-      if ( informationTable.querySelector('tr:last-child td').getAttribute('colspan') === '4' ) {
+      const information = informationTable.querySelectorAll('tbody > tr > td')
+
+      if ( information.length === 1 ) {
         return reject({
           code: 404,
-          mesage: informationTable.querySelector('tr:last-child td').innerHTML,
+          message: information[0].innerHTML
         })
       }
 
       let shippingInformation = {
-        from: {time: null},
-        to: {time: null},
+        from: { time: null, name: information[1].textContent},
+        to: {time: null, name: information[2].textContent},
         state: {},
         progresses: ((table) => {
           let result = []
-          table.querySelectorAll('tr').forEach(element => {
+          table.querySelectorAll('tbody > tr').forEach(element => {
             const tds = element.querySelectorAll('td')
-            if ( tds.length === 0 ) { return }
-            if ( tds[1].innerHTML == '--:--' ) { return }
+
+            if (tds[1].textContent.indexOf('--:--') !== -1) return;
+            
             result.push({
-              time: `${tds[0].innerHTML.replace(/\./g, '-')}T${tds[1].innerHTML}:00+09:00`,
+              status: parseStatus(tds[0].textContent),
+              time: `${tds[1].textContent.replace(/\s+/g, 'T')}:00+09:00`,
               location: {
-                name: trimString(tds[2].textContent),
+                name: tds[2].textContent,
               },
-              status: parseStatus(tds[3].textContent),
-              description: trimString(tds[3].textContent),
+              description: tds[3].textContent,
             })
           })
           return result
-        })(progressTable),
+        })(progressTable)
       }
 
       if(shippingInformation.progresses.length < 1) {
@@ -82,6 +68,7 @@ function getTrack(trackId) {
       }
 
       resolve(shippingInformation)
+
     }).catch(err => reject(err))
   })
 }
@@ -93,5 +80,3 @@ module.exports = {
   },
   getTrack: getTrack,
 }
-
-// getTrack('401041508125').then(res => console.log(JSON.stringify(res, 0, 2))).catch(err => console.log(err))
