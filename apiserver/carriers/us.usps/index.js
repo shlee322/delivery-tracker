07f23f4b2d1974141bf91f13d915139d2aef0246
Kyle Sanghyeok Lee <shlee322@gmail.com>
2019-07-24 13:12:26 +0900

    us.usps - Fixed day padding bug (#42)

--- apiserver/carriers/us.usps/index.js
+++ apiserver/carriers/us.usps/index.js
@@ -13,7 +13,7 @@ function getTime(location, time) {
 
   const year = dateObj.getFullYear()
   const month = String(dateObj.getMonth()+1).padStart(2, '0')
-  const day = dateObj.getDate()
+  const day = String(dateObj.getDate()).padStart(2, '0')
 
   const hours =  String(dateObj.getHours()).padStart(2, '0')
   const minutes =  String(dateObj.getMinutes()).padStart(2, '0')
