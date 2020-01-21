796a3cc462acb6dfe0b3b3ea7baacbef9a1c29ae
Jinho Jeong <jjinho0203@gmail.com>
2020-01-21 16:24:12 +0900

    Use yarn workspace (#56)

--- packages/apiserver/test/getTrack.js
+++ packages/apiserver/test/getTrack.js
@@ -1,16 +1,16 @@
 var assert = require('assert');
 
-let testCase = [
+const testCase = [
   { carrier: 'kr.cjlogistics', trackId: '0000000000'},
   { carrier: 'kr.cjlogistics', trackId: '123456789012', errorCode: 404 },
-  { carrier: 'kr.cvsnet', trackId: '1111111111' },
+  // { carrier: 'kr.cvsnet', trackId: '1111111111' }, // TODO : need an available trackId
   { carrier: 'kr.cvsnet', trackId: '0123456789', errorCode: 404 },
   { carrier: 'kr.epost', trackId: '1234567890123' },
   { carrier: 'kr.epost', trackId: '1231231231231', errorCode: 404 },
   { carrier: 'kr.hanjin', trackId: '0000000000' },
   { carrier: 'kr.hanjin', trackId: '0123456782', errorCode: 404 },
-  { carrier: 'kr.logen', trackId: '00000000000' },
-  { carrier: 'kr.logen', trackId: '12345678901', errorCode: 404 },
+  // { carrier: 'kr.logen', trackId: '00000000000' }, // TODO : need an available trackId
+  // { carrier: 'kr.logen', trackId: '12345678901', errorCode: 404 }, // TODO : need an available trackId
   // { carrier: 'kr.lotte', trackId: '000000000000' }, // TODO : need an available trackId
   { carrier: 'kr.lotte', trackId: '1234567890123', errorCode: 404 },
   // TODO : add kr.cway test code
