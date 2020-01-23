26ad4fc0a9647f055d2076dadbe2579009147ddd
Jinho Jeong <jjinho0203@gmail.com>
2020-01-23 21:38:53 +0900

    fix lint

--- packages/client/pages/index.vue
+++ packages/client/pages/index.vue
@@ -12,8 +12,12 @@
         <h1 v-else class="has-text-centered">추적실패</h1>
       </div>
       <div class="tracking-illustration has-text-centered">
-        <img v-if="error === null" src="@/assets/nocanvas.png" alt="No Canvas">
-        <img v-else src="@/assets/nocanvas_failed.png" alt="No Canvas Failed">
+        <img
+          v-if="error === null"
+          src="@/assets/nocanvas.png"
+          alt="No Canvas"
+        />
+        <img v-else src="@/assets/nocanvas_failed.png" alt="No Canvas Failed" />
       </div>
       <b-message type="is-danger" v-if="error !== null">
         {{ error.message }}
@@ -22,7 +26,11 @@
     <div v-if="error === null" class="tracking-detail">
       <section class="tracking-progresses">
         <transition-group name="fold">
-          <div v-for="(progressesByDate, date) in progresses" :key="date" class="tracking-progress-entry">
+          <div
+            v-for="(progressesByDate, date) in progresses"
+            :key="date"
+            class="tracking-progress-entry"
+          >
             <time>{{ beautifyDate(date) }}</time>
             <ul>
               <li v-for="progress in progressesByDate" :key="progress.id">
@@ -39,9 +47,15 @@
           </div>
         </transition-group>
         <transition-group v-show="!isProgressTooShort" name="fold">
-          <a class="fold-button has-text-centered" :key="`test1`" @click="fold = !fold">
+          <a
+            class="fold-button has-text-centered"
+            :key="`test1`"
+            @click="fold = !fold"
+          >
             <div v-if="fold">더보기&nbsp;<i class="fas fa-angle-down"></i></div>
-            <div v-else>간략히&nbsp;보기&nbsp;<i class="fas fa-angle-up"></i></div>
+            <div v-else>
+              간략히&nbsp;보기&nbsp;<i class="fas fa-angle-up"></i>
+            </div>
           </a>
         </transition-group>
         <div v-show="isProgressTooShort" class="fold-button"></div>
@@ -57,7 +71,11 @@
           </li>
           <li>
             <h5>택배사</h5>
-            <p>{{ carrier.name }} ({{ formatNumber(carrier.tel, 'International') }})</p>
+            <p>
+              {{ carrier.name }} ({{
+                formatNumber(carrier.tel, 'International')
+              }})
+            </p>
           </li>
           <li>
             <h5>송장번호</h5>
@@ -71,15 +89,23 @@
       </section>
     </div>
     <div style="margin-top:20px; color: #585858; font-size: 0.8125rem;">
-      <a class="github-button" href="https://github.com/shlee322/delivery-tracker" data-icon="octicon-star" data-show-count="true" aria-label="Star shlee322/delivery-tracker on GitHub">Star</a>
-      Copyright (C) <a href="https://tracker.delivery">https://tracker.delivery</a>
+      <a
+        class="github-button"
+        href="https://github.com/shlee322/delivery-tracker"
+        data-icon="octicon-star"
+        data-show-count="true"
+        aria-label="Star shlee322/delivery-tracker on GitHub"
+        >Star</a
+      >
+      Copyright (C)
+      <a href="https://tracker.delivery">https://tracker.delivery</a>
       <script async defer src="https://buttons.github.io/buttons.js"></script>
     </div>
   </div>
 </template>
 
 <script>
-import { formatNumber } from 'libphonenumber-js'
+import { formatNumber } from 'libphonenumber-js';
 
 export default {
   data: () => ({
@@ -101,87 +127,104 @@ export default {
     error: null,
   }),
   beforeMount() {
-    this.fetchData()
+    this.fetchData();
   },
   watch: {
     $route() {
-      this.fetchData()
+      this.fetchData();
     },
     fold() {
-      this.progresses = this.fold && !this.isProgressTooShort ?
-        this.getProgressesByDate(this.info.progresses.slice(this.info.progresses.length-3))
-        : this.getProgressesByDate(this.info.progresses)
-    }
+      this.progresses =
+        this.fold && !this.isProgressTooShort
+          ? this.getProgressesByDate(
+              this.info.progresses.slice(this.info.progresses.length - 3)
+            )
+          : this.getProgressesByDate(this.info.progresses);
+    },
   },
   methods: {
     beautifyDate(date) {
-      return date.replace(/\-/g, '.')
+      return date.replace(/-/g, '.');
     },
     getTime(isoDate) {
-      let time = ''
+      let time = '';
       try {
         time = new Date(isoDate).toLocaleString('ko-KR', {
           hour: 'numeric',
           minute: 'numeric',
-        })
+        });
       } catch (error) {
-        time = '알 수 없는 시간'
+        time = '알 수 없는 시간';
       }
-      return time
+      return time;
     },
     formatNumber,
     fetchData() {
-      let carrierName, track
+      let carrierName;
+      let track;
       try {
-        [, carrierName, track] = /#!?\/(\w+.\w+)\/(\d+)/.exec(this.$route.hash)
+        [, carrierName, track] = /#!?\/(\w+.\w+)\/(\d+)/.exec(this.$route.hash);
       } catch (error) {
-        this.goGuide()
-        return
+        this.goGuide();
+        return;
       }
-      this.isLoading = true
-      this.error = null
+      this.isLoading = true;
+      this.error = null;
 
       this.$axios
         .get(`/carriers/${carrierName}/tracks/${track}`)
         .then(res => {
-          this.carrier = res.data.carrier
-          this.track = track
-          this.info = res.data
-          return res.data.progresses
+          this.carrier = res.data.carrier;
+          this.track = track;
+          this.info = res.data;
+          return res.data.progresses;
         })
         .then(res => {
-          this.isProgressTooShort = res.length <= 3
-          this.progresses = this.getProgressesByDate(this.isProgressTooShort ? res: res.slice(res.length-3))
-          this.isLoading = false
+          this.isProgressTooShort = res.length <= 3;
+          this.progresses = this.getProgressesByDate(
+            this.isProgressTooShort ? res : res.slice(res.length - 3)
+          );
+          this.isLoading = false;
         })
         .catch(err => {
           this.error = {
             statusCode: err.response.status,
-            message: err.response.data.message ? err.response.data.message : '알 수 없는 에러입니다!',
-          }
-          this.isLoading = false
-        })
+            message: err.response.data.message
+              ? err.response.data.message
+              : '알 수 없는 에러입니다!',
+          };
+          this.isLoading = false;
+        });
     },
     getProgressesByDate(p) {
-      let result = {}
-      p.slice().sort((a, b) => {
-        return a.time > b.time ? -1 : a.time < b.time ? 1 : 0
-      })
-      .forEach(progress => {
-        const key = progress.time.substr(0, 10)
-        if (!(key in result)) {
-          result[key] = []
-        }
-        result[key].push(progress)
-      })
-      return result
+      const result = {};
+      p.slice()
+        .sort((a, b) => {
+          if (a.time > b.time) {
+            return -1;
+          }
+
+          if (a.time < b.time) {
+            return 1;
+          }
+
+          return 0;
+        })
+        .forEach(progress => {
+          const key = progress.time.substr(0, 10);
+          if (!(key in result)) {
+            result[key] = [];
+          }
+          result[key].push(progress);
+        });
+      return result;
     },
     goGuide() {
-      alert('가이드로 이동합니다.')
-      this.$router.replace('/guide')
+      window.alert('가이드로 이동합니다.'); // eslint-disable-line no-alert
+      this.$router.replace('/guide');
     },
   },
-}
+};
 </script>
 
 <style lang="scss" scoped>
@@ -195,8 +238,9 @@ $wide-letter-spacing: 0.25rem;
   transform: translateY(-50%);
 }
 
-.fold-enter-active, .fold-leave-active {
-  transition: display .9s, opacity .6.5s ease-in-out;
+.fold-enter-active,
+.fold-leave-active {
+  transition: display 0.9s, opacity 0.6.5s ease-in-out;
 }
 
 .fold-enter, .fold-leave-to /* .fade-leave-active below version 2.1.8 */ {
