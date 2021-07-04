2ce139311d2a343be2e1e2f898957ba9e544f5ce
Do it for fun <me@falsy.me>
2021-07-05 01:42:05 +0900

    한진택배 API 스크래핑 URL 및 데이터 인코딩 오류 수정

--- packages/apiserver/carriers/kr.hanjin/index.js
+++ packages/apiserver/carriers/kr.hanjin/index.js
@@ -1,5 +1,7 @@
 const axios = require('axios');
 const { JSDOM } = require('jsdom');
+const { Iconv } = require('iconv');
+const iconv = new Iconv('euc-kr', 'utf-8');
 
 function parseStatus(s) {
   if (s.includes('집하')) return { id: 'at_pickup', text: '상품인수' };
@@ -30,17 +32,15 @@ function getTrack(trackId) {
     }
 
     axios
-      .get('http://www.hanjin.co.kr/kor/CMS/DeliveryMgr/WaybillResult.do', {
+      .get('http://www.hanjinexpress.hanjin.net/customer/hddcw18.tracking', {
         params: {
-          mCode: 'MN038',
-          schLang: 'KR',
-          wblnum: trackId,
-        }
+          w_num: trackId,
+        },
+        responseType: 'arraybuffer'
       })
       .then(res => {
-        const dom = new JSDOM(res.data);
+        const dom = new JSDOM(iconv.convert(res.data).toString());
         const tables = dom.window.document.querySelectorAll('table');
-        console.log(tables)
         
         if (tables.length === 0) {
           return reject({
@@ -48,63 +48,61 @@ function getTrack(trackId) {
             message: dom.window.document.querySelector('.noData').textContent,
           });
         }
-        resolve(true)
-        // return { informationTable: tables[0], progressTable: tables[1] };
+        return { informationTable: tables[0], progressTable: tables[1] };
       })
-      // .then(({ informationTable, progressTable }) => {
+      .then(({ informationTable, progressTable }) => {
 
-      //   const td = informationTable.querySelectorAll('td');
+        const td = informationTable.querySelectorAll('td');
+        const shippingInformation = {
+          from: {
+            name: td[1].textContent,
+            time: null,
+          },
+          to: {
+            name: td[2].textContent,
+            time: null,
+          },
+          state: {
+            id: 'delivered',
+            text: null,
+          },
+          progresses: [],
+        };
 
-      //   const shippingInformation = {
-      //     from: {
-      //       name: td[1].textContent,
-      //       time: null,
-      //     },
-      //     to: {
-      //       name: td[2].textContent,
-      //       time: null,
-      //     },
-      //     state: {
-      //       id: 'delivered',
-      //       text: null,
-      //     },
-      //     progresses: [],
-      //   };
+        const { progresses } = shippingInformation;
 
-      //   const { progresses } = shippingInformation;
-
-      //   progressTable.querySelector('tbody').querySelectorAll('tr').forEach(element => {
-      //     const insideTd = element.querySelectorAll('td');
-      //     const date = insideTd[0].textContent; // insideTd[0] - 날짜 (ex. 2021-04-13)
-      //     const time = insideTd[1].textContent; // insideTd[1] - 시간 (ex. 10:37)
-      //     const address = insideTd[2].textContent; // insideTd[2] - 위치
-      //     const description = insideTd[3].textContent;// insideTd[3] - 설명
-      //     const timeSet = `${date}T${time}:00+09:00`;
+        progressTable.querySelector('tbody').querySelectorAll('tr').forEach(element => {
+          const insideTd = element.querySelectorAll('td');
+          const date = insideTd[0].textContent; // insideTd[0] - 날짜 (ex. 2021-04-13)
+          const time = insideTd[1].textContent; // insideTd[1] - 시간 (ex. 10:37)
+          const address = insideTd[2].textContent; // insideTd[2] - 위치
+          const description = insideTd[3].textContent.trim();// insideTd[3] - 설명
+          const timeSet = `${date}T${time}:00+09:00`;
           
-      //     progresses.unshift({
-      //       time: timeSet,
-      //       location: {
-      //         name: address,
-      //       },
-      //       status: parseStatus(description),
-      //       description: description,
-      //     });
-      //   });
+          progresses.unshift({
+            time: timeSet,
+            location: {
+              name: address,
+            },
+            status: parseStatus(description),
+            description: description,
+          });
+        });
 
-      //   if (progresses.length > 0) {
-      //     shippingInformation.state = progresses[0].status;
-      //     shippingInformation.from.time = progresses[progresses.length - 1].time;
-      //     if (progresses[0].status.id === 'delivered')
-      //       shippingInformation.to.time = progresses[0].time;
-      //   } else {
-      //     shippingInformation.state = {
-      //       id: 'information_received',
-      //       text: '방문예정',
-      //     };
-      //   }
+        if (progresses.length > 0) {
+          shippingInformation.state = progresses[0].status;
+          shippingInformation.from.time = progresses[progresses.length - 1].time;
+          if (progresses[0].status.id === 'delivered')
+            shippingInformation.to.time = progresses[0].time;
+        } else {
+          shippingInformation.state = {
+            id: 'information_received',
+            text: '방문예정',
+          };
+        }
 
-      //   resolve(shippingInformation);
-      // })
+        resolve(shippingInformation);
+      })
       .catch(err => reject(err));
   });
 }
