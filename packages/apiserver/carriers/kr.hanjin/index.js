4f212da60aeb3b5dd0774979ad86195b3938efe3
Do it for fun <me@falsy.me>
2021-07-04 23:47:30 +0900

    서버에서 403 오류 원인 확인1

--- packages/apiserver/carriers/kr.hanjin/index.js
+++ packages/apiserver/carriers/kr.hanjin/index.js
@@ -36,6 +36,7 @@ function getTrack(trackId) {
       .then(res => {
         const dom = new JSDOM(res.data);
         const tables = dom.window.document.querySelectorAll('table');
+        console.log(tables)
         
         if (tables.length === 0) {
           return reject({
@@ -43,63 +44,63 @@ function getTrack(trackId) {
             message: dom.window.document.querySelector('.noData').textContent,
           });
         }
-
-        return { informationTable: tables[0], progressTable: tables[1] };
+        resolve(true)
+        // return { informationTable: tables[0], progressTable: tables[1] };
       })
-      .then(({ informationTable, progressTable }) => {
+      // .then(({ informationTable, progressTable }) => {
 
-        const td = informationTable.querySelectorAll('td');
+      //   const td = informationTable.querySelectorAll('td');
 
-        const shippingInformation = {
-          from: {
-            name: td[1].textContent,
-            time: null,
-          },
-          to: {
-            name: td[2].textContent,
-            time: null,
-          },
-          state: {
-            id: 'delivered',
-            text: null,
-          },
-          progresses: [],
-        };
+      //   const shippingInformation = {
+      //     from: {
+      //       name: td[1].textContent,
+      //       time: null,
+      //     },
+      //     to: {
+      //       name: td[2].textContent,
+      //       time: null,
+      //     },
+      //     state: {
+      //       id: 'delivered',
+      //       text: null,
+      //     },
+      //     progresses: [],
+      //   };
 
-        const { progresses } = shippingInformation;
+      //   const { progresses } = shippingInformation;
 
-        progressTable.querySelector('tbody').querySelectorAll('tr').forEach(element => {
-          const insideTd = element.querySelectorAll('td');
-          const date = insideTd[0].textContent; // insideTd[0] - 날짜 (ex. 2021-04-13)
-          const time = insideTd[1].textContent; // insideTd[1] - 시간 (ex. 10:37)
-          const address = insideTd[2].textContent; // insideTd[2] - 위치
-          const description = insideTd[3].textContent;// insideTd[3] - 설명
-          const timeSet = `${date}T${time}:00+09:00`;
+      //   progressTable.querySelector('tbody').querySelectorAll('tr').forEach(element => {
+      //     const insideTd = element.querySelectorAll('td');
+      //     const date = insideTd[0].textContent; // insideTd[0] - 날짜 (ex. 2021-04-13)
+      //     const time = insideTd[1].textContent; // insideTd[1] - 시간 (ex. 10:37)
+      //     const address = insideTd[2].textContent; // insideTd[2] - 위치
+      //     const description = insideTd[3].textContent;// insideTd[3] - 설명
+      //     const timeSet = `${date}T${time}:00+09:00`;
           
-          progresses.unshift({
-            time: timeSet,
-            location: {
-              name: address,
-            },
-            status: parseStatus(description),
-            description: description,
-          });
-        });
+      //     progresses.unshift({
+      //       time: timeSet,
+      //       location: {
+      //         name: address,
+      //       },
+      //       status: parseStatus(description),
+      //       description: description,
+      //     });
+      //   });
 
-        if (progresses.length > 0) {
-          shippingInformation.state = progresses[0].status;
-          shippingInformation.from.time = progresses[progresses.length - 1].time;
-          if (progresses[0].status.id === 'delivered')
-            shippingInformation.to.time = progresses[0].time;
-        } else {
-          shippingInformation.state = {
-            id: 'information_received',
-            text: '방문예정',
-          };
-        }
+      //   if (progresses.length > 0) {
+      //     shippingInformation.state = progresses[0].status;
+      //     shippingInformation.from.time = progresses[progresses.length - 1].time;
+      //     if (progresses[0].status.id === 'delivered')
+      //       shippingInformation.to.time = progresses[0].time;
+      //   } else {
+      //     shippingInformation.state = {
+      //       id: 'information_received',
+      //       text: '방문예정',
+      //     };
+      //   }
 
-        resolve(shippingInformation);
-      })
+      //   resolve(shippingInformation);
+      // })
       .catch(err => reject(err));
   });
 }
